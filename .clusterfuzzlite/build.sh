fcfefff17d7c753482e9807ed0237995d7f481f9
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-08 17:56:14 +0700

    ci(fuzz): add ClusterFuzzLite build script

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eu
+
+# this script is executed by clusterfuzzlite.
+# $CC, $CFLAGS, $LIB_FUZZING_ENGINE, and $OUT are provided by the environment.
+
+# build the fuzzer executable linking against our source and the fuzzing engine.
+$CC $CFLAGS -Iinclude -Isrc -DCJSONX_IMPLEMENTATION src/cjsonx.c tests/fuzzer.c -o $OUT/fuzzer $LIB_FUZZING_ENGINE
