08ce002b62d9588bc03a833fefbaf68a5e370ce7
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-08 18:00:22 +0700

    scripts: add generate_table.c skeleton code

---
+++ scripts/generate_table.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+int main() {
+    printf("static const uint64_t cjsonx_eisel_lemire_mantissas[] = {\n");
+    // ... we can just generate it directly in python
+}
