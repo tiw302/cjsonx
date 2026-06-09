31b1d5211360d0bb537845085f23f55823679b04
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-09 20:24:58 +0700

    ci(fuzz): remove redundant compiler macro flag from fuzzer build script

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,4 +4,4 @@
 # $CC, $CFLAGS, $LIB_FUZZING_ENGINE, and $OUT are provided by the environment.
 
 # build the fuzzer executable linking against our source and the fuzzing engine.
-$CC $CFLAGS -Iinclude -Isrc -DCJSONX_IMPLEMENTATION src/cjsonx.c tests/fuzzer.c -o $OUT/fuzzer $LIB_FUZZING_ENGINE
+$CC $CFLAGS -Iinclude -Isrc src/cjsonx.c tests/fuzzer.c -o $OUT/fuzzer $LIB_FUZZING_ENGINE
