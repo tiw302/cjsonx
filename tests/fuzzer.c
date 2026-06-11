a71506250db4bbc19e0e0a9e25fda06c5ed471dc
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-11 22:06:43 +0700

    test(fuzzer): expand coverage to DOM builder and value cloning
    * Inject key builder mutations and clone routines into LLVM fuzzer loop to verify arena stability

--- tests/fuzzer.c
+++ tests/fuzzer.c
@@ -23,6 +23,27 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
                 (void)type;
             }
             
+            // test builder functions
+            cjsonx_val_t root = doc->root;
+            cjsonx_type_t root_type = cjsonx_get_type(root);
+            if (root_type == CJSONX_OBJECT) {
+                cjsonx_val_t val = cjsonx_create_bool(doc, true);
+                cjsonx_object_set(root, "fuzz_key", val);
+                cjsonx_object_set(root, "fuzz_key", cjsonx_create_null(doc));
+                cjsonx_object_remove(root, "fuzz_key");
+            } else if (root_type == CJSONX_ARRAY) {
+                cjsonx_val_t val = cjsonx_create_number(doc, 42.0);
+                cjsonx_array_push(root, val);
+                cjsonx_array_remove(root, cjsonx_size(root) - 1);
+            }
+            // test value cloning
+            cjsonx_doc_t* cloned_doc = cjsonx_doc_new();
+            if (cloned_doc) {
+                cjsonx_val_t cloned_val = cjsonx_clone_val(cloned_doc, root);
+                (void)cloned_val;
+                cjsonx_doc_free(cloned_doc);
+            }
+
             // test stringify
             char* str = cjsonx_stringify(doc);
             if (str) {
