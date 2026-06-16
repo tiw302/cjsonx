01d7bfc1d04d74428f1821d38b97528c7c6454e5
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-16 07:03:13 +0700

    build(scripts): remove deprecated table generation source files

--- scripts/generate_table.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-int main() {
-    printf("static const uint64_t cjsonx_eisel_lemire_mantissas[] = {\n");
-    // ... we can just generate it directly in python
-}
