f153df4aa9ff21f519a7cd25668b1eae126ca205
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-16 07:03:12 +0700

    build(benchmarks): pin third-party download URLs to specific tags

--- benchmarks/download_third_party.sh
+++ benchmarks/download_third_party.sh
@@ -6,15 +6,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p "$DIR/third_party"
 cd "$DIR/third_party"
 
-echo "Downloading cJSON..."
-curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/master/cJSON.c
-curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/master/cJSON.h
+echo "Downloading cJSON v1.7.18..."
+curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.c
+curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.h
 
-echo "Downloading jsmn..."
-curl -L -O https://raw.githubusercontent.com/zserge/jsmn/master/jsmn.h
+echo "Downloading jsmn v1.1.0..."
+curl -L -O https://raw.githubusercontent.com/zserge/jsmn/v1.1.0/jsmn.h
 
-echo "Downloading yyjson..."
-curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/master/src/yyjson.c
-curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/master/src/yyjson.h
+echo "Downloading yyjson 0.10.0..."
+curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.c
+curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.h
 
 echo "Done downloading third party libraries."
