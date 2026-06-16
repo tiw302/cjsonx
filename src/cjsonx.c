6d39ced422b7a2354bd09b676e5a9782ab520177
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-06-16 07:03:13 +0700

    style(src): add license header to src/cjsonx.c

--- src/cjsonx.c
+++ src/cjsonx.c
@@ -1,3 +1,7 @@
+// updated 2026-06-13
+// spdx-license-identifier: mit
+// copyright (c) 2026 jirawat siripuk
+
 // compile the implementation from the modular headers
 #define CJSONX_IMPLEMENTATION
 #include "../include/cjsonx.h"
