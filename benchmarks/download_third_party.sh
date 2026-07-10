fd73287d1ee5a69c6a052047b0e65360ceb4c93a
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-10 20:51:41 +0700

    test(bench): add script description header and use curl fail flag

--- benchmarks/download_third_party.sh
+++ benchmarks/download_third_party.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# =========================================================================
+# file: download_third_party.sh
+# description: utility script to fetch specific, pinned versions of competitor
+#              json libraries for fair and reproducible benchmarking.
+# =========================================================================
 set -e
 
 # get the directory of the script
@@ -7,14 +12,14 @@ mkdir -p "$DIR/third_party"
 cd "$DIR/third_party"
 
 echo "Downloading cJSON v1.7.18..."
-curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.c
-curl -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.h
+curl -f -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.c
+curl -f -L -O https://raw.githubusercontent.com/DaveGamble/cJSON/v1.7.18/cJSON.h
 
 echo "Downloading jsmn v1.1.0..."
-curl -L -O https://raw.githubusercontent.com/zserge/jsmn/v1.1.0/jsmn.h
+curl -f -L -O https://raw.githubusercontent.com/zserge/jsmn/v1.1.0/jsmn.h
 
 echo "Downloading yyjson 0.10.0..."
-curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.c
-curl -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.h
+curl -f -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.c
+curl -f -L -O https://raw.githubusercontent.com/ibireme/yyjson/0.10.0/src/yyjson.h
 
 echo "Done downloading third party libraries."
