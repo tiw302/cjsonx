d93afd3ce4a8ee6043aff3e9c38e06b0a5e120e8
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-10 20:51:41 +0700

    build(python): add pathlib and load long description from README

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, Extension
 import pybind11
 
 import platform
+from pathlib import Path
+
+# read the readme file for the long description
+this_directory = Path(__file__).parent
+long_desc = (this_directory / "README.md").read_text(encoding="utf-8")
 
 extra_compile_args = []
 if sys.platform == 'win32':
@@ -26,6 +31,8 @@ setup(
     name='cjsonx',
     version='1.3.0',
     description="High-performance JSON parser C11 bindings for Python",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
     packages=[],
     zip_safe=False,
