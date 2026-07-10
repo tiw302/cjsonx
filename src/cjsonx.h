b8cc754911dbedf85709c862752dd5bdba412032
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-07-10 20:51:42 +0700

    chore(core): update file modified date and add ASCII art header

--- src/cjsonx.h
+++ src/cjsonx.h
@@ -1,11 +1,25 @@
-// updated 2026-06-13
+// updated 2026-07-09
 // spdx-license-identifier: mit
 // copyright (c) 2026 jirawat siripuk
+
+/*
+ *       /\_/\
+ *      ( -.- )  <-- "parsing json at 3 gb/s. developer sanity: 0 bytes."
+ *     ==  y  ==
+ *      (  .  )
+ *      (  .  )
+ */
 
 #ifndef CJSONX_SRC_H
 #define CJSONX_SRC_H
 
-// forward to the modular entry header
+// ███████ ██████   ██████
+// ██      ██   ██ ██
+// ███████ ██████  ██
+//      ██ ██   ██ ██
+// ███████ ██   ██  ██████
+//
+// >>forward to the modular entry header
 #include "../include/cjsonx.h"
 
 #endif  // cjsonx_src_h
